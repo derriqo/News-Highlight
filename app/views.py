14c70d89f49e7087c1fa0324c351473877ec750b
derrrick william <williamderrick100@gmail.com>
2019-02-05 08:27:24 +0300

    Added additional sources.

--- app/views.py
+++ app/views.py
@@ -10,8 +10,12 @@ def index():
     View root page function that returns the index page and its data
     '''
     inside = get_sources('general')
+    world_sources = get_sources('world')
+    top_sources = get_sources('top')
+
+
     word = 'Welcome to News'
-    return render_template('index.html', word=word, inside=inside )
+    return render_template('index.html', word=word, inside=inside, world_sources = world_sources, top_sources=top_sources )
 
 # @app.route('/articles/<id>')
 # def articles(id)
