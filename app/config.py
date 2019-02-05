e21d893d248b73b75be1399070dc64174f39b9ea
derrrick william <williamderrick100@gmail.com>
2019-02-05 10:37:25 +0300

    Corrected errors with the Api key.

--- app/config.py
+++ app/config.py
@@ -2,5 +2,32 @@ class Config:
     '''
     General configuration parent class
     '''
-    NEWS_API_URL  ='https://newsapi.org/v2/everything/{}?api_key={}'
+    SOURCE_API_URL  ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    SOURCE_API_KEY = os.environ.get('SOURCE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
